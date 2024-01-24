b83b5f97cf969ad0fa0b85a690462c7b8ba5f386
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 11:35:46 +0900

    Hell -> Hello 수정

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 //	 프로그램에서 프로그램 시작부분을 main으로 정해놓았다.
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		System.out.println("Hell, Java");
-		System.out.println("Hell, Korea");
+		System.out.println("Hello, Java");
+		System.out.println("Hello, Korea");
 	}
 }
