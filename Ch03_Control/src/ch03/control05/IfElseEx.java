135c1f77454d382b6dbb7c7bdbd8eded0b00c60b
cbm1801 <cbm1801@naver.com>
2024-01-25 15:31:09 +0900

    문제 해답

--- Ch03_Control/src/ch03/control05/IfElseEx.java
+++ Ch03_Control/src/ch03/control05/IfElseEx.java
@@ -13,21 +13,20 @@ public class IfElseEx {
 				
 		Scanner sc = new Scanner(System.in);
 		int sel = sc.nextInt();
-		
-		if(sel >= 6)
-			System.out.println("해당 메뉴가 없습니다.");
-		else if(sel >= 5)
-			System.out.println("유린기가 나왔습니다.");
-		else if(sel >= 4)
-			System.out.println("유산슬이 나왔습니다.");
-		else if(sel >= 3)
-			System.out.println("탕수육이 나왔습니다.");
-		else if(sel >= 2)
-			System.out.println("짬뽕이 나왔습니다.");
+		// switch -> ifelse문으로 전환하세요.
+		if(sel==1)
+			System.out.println("짜장면이 나왔습니다");			
+		else if(sel==2)
+			System.out.println("짬뽕이 나왔습니다");			
+		else if(sel==3)
+			System.out.println("탕수육이 나왔습니다");			
+		else if(sel==4)
+			System.out.println("유산슬이 나왔습니다");
+		else if(sel==5)
+			System.out.println("유린기가 나왔습니다");
 		else
-			System.out.println("짜장면이 나놨습니다.");
+			System.out.println("해당 메뉴 번호가 없습니다");
 		
 		sc.close();
-		
 	}
 }
