9345ce4f32d4d0b8e92c9a21a33aa88667e6f11e
cbm1801 <cbm1801@naver.com>
2024-01-25 12:51:58 +0900

    OrderOperator.java 연산자 우선순위 예제

---
+++ Ch02_Variable/src/ch02/orderOpeator13/OrderOperator.java
@@ -0,0 +1,13 @@
+package ch02.orderOpeator13;
+
+public class OrderOperator {
+
+	public static void main(String[] args) {
+		int result = 10 + 50 * 3;
+		System.out.println(result);
+		result = (10 + 50) * 3;
+		System.out.println(result);
+
+	}
+
+}
