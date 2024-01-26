30f763732fbb3c6724723bdb3d3ec30b76ee700d
cbm1801 <cbm1801@naver.com>
2024-01-26 17:51:42 +0900

    문제 해답

--- Ch05_Array/src/ch05/array08/Resolve2.java
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -1,7 +1,19 @@
 package ch05.array08;
 
+import java.util.Arrays;
+
 //2.정수배열 5개를 할당하고
 //Arrays.sort를 사용하지 말고 최소값과 최대값을 얻으세요
 public class Resolve2 {
+	public static void main(String[] args) {
+		
+	
+	int[] num = {1542151,232,436,23,13};
+	
+	Arrays.parallelSort(num);
+	
+	System.out.println(Arrays.toString(num));
+	
+	}
 
 }
