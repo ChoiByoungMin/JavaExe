bcb956f26c77fcca5b62ca284cf82c0db2d2db1d
cbm1801 <cbm1801@naver.com>
2024-01-26 11:09:46 +0900

    해답 풀이 추가

--- Ch03_Control/src/ch03/for10/Resolve15.java
+++ Ch03_Control/src/ch03/for10/Resolve15.java
@@ -28,5 +28,43 @@ public class Resolve15 {
 		sc.close();
 
 	}
+/*
+ * 		public static void method0() {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		while(true) {
+			System.out.print("정수 입력(0은 합계 출력) >> ");
+			int num = sc.nextInt();
+			if(num == 0)
+				break;
+			sum += num;
+		}
+		System.out.println("누적합은 " + sum);
+		 
+		sc.close();
+	}
+
+	public static void method1() {
+		Scanner sc = new Scanner(System.in);
+		
+		boolean isRun = true;
+		int sum = 0;
+		while(isRun) {
+			System.out.print("정수 입력(0은 합계 출력) >> ");
+			int num = sc.nextInt();
+			if(num == 0)
+				isRun = false;
+			sum += num;
+		}
+		System.out.println("누적합은 " + sum);
+		 
+		sc.close();
+	}
+
+	public static void main(String[] args) {
+//		method0();
+		method1();
+ */
 
 }
