bc790464bdb53abb5c633ad419dd103cd14e7f82
cbm1801 <cbm1801@naver.com>
2024-01-26 13:29:29 +0900

    3문제 풀이

--- Ch03_Control/src/ch03/continue11/Resolve3.java
+++ Ch03_Control/src/ch03/continue11/Resolve3.java
@@ -9,23 +9,36 @@ public class Resolve3 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int a =1, b=2, c=3;
-		
 		while(true) {
 			System.out.println("--------------------------");
 			System.out.println("1.예금 | 2.출금 3.잔고 | 4.종료");
 			System.out.println("--------------------------");
 			System.out.println();
-			System.out.println("선택>");
+			System.out.print("선택>");
 			
-			int stop = sc.nextInt();
+			int a = sc.nextInt();
 			
+			if (a==1) {
+				System.out.println("예금액>"+10000);
+				System.out.println();
+			}	
+			else if (a==2) {
+				System.out.println("출금액>"+2000);
+				System.out.println();
+			}
+			else if (a==3) {
+				System.out.println("잔고>"+8000);
+				System.out.println();
+			}
 			
+			else if(a == 4) 
+				break;
 			
 		}
+		System.out.println();
+		System.out.println("프로그램 종료");
 		
-		
-		
+		sc.close();
 	}
 
 }
