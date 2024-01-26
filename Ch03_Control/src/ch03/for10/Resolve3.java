0a33c588c5a4f878500746a60f7b94189e1d27cb
cbm1801 <cbm1801@naver.com>
2024-01-26 10:28:19 +0900

    3번 풀이 해석

--- Ch03_Control/src/ch03/for10/Resolve3.java
+++ Ch03_Control/src/ch03/for10/Resolve3.java
@@ -27,5 +27,64 @@ public class Resolve3 {
 		sc.close();
 	
 	}
+/*
+ * 	public static void methodIf() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1번째 숫자 입력 >> ");
+		int num0 = sc.nextInt();
+		System.out.print("2번째 숫자 입력 >> ");
+		int num1 = sc.nextInt();
+		System.out.print("3번째 숫자 입력 >> ");
+		int num2 = sc.nextInt();
+		
+		boolean bTriangle = false;
+//		if((num0+num1 > num2) && 
+//			(num1+num2 > num0) &&
+//			(num2+num0 > num1))
+//			bTriangle = true;
+		
+		if(num0+num1 > num2) {
+			if(num1+num2 > num0) {
+				if(num2+num0 > num1)
+					bTriangle = true;
+			}
+		}
+		System.out.println("삼각형의 조건이 " + bTriangle + "입니다");
+		
+		sc.close();
+	}
+	
+	public static void methodCondOp(){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1번째 숫자 입력 >> ");
+		int num0 = sc.nextInt();
+		System.out.print("2번째 숫자 입력 >> ");
+		int num1 = sc.nextInt();
+		System.out.print("3번째 숫자 입력 >> ");
+		int num2 = sc.nextInt();
+		
+		boolean bTriangle = false;
+		
+		boolean cond0 = num0+num1 > num2;
+		boolean cond1 = num1+num2 > num0;
+		boolean cond2 = num2+num0 > num1;
+		
+		bTriangle = cond0 ? (cond1 ? (cond2 ? true : false) 
+				                   : false) 
+				          : false; 				
+		
+
+		System.out.println("삼각형의 조건이 " + bTriangle + "입니다");
+		
+		sc.close();		
+	}
+	
+	public static void main(String[] args) {
+//		methodIf();
+		methodCondOp();
+	}
+ */
 
 }
