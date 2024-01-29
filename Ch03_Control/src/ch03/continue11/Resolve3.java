d93f6edf43f79be0e5d94980485d37e2b3047af8
cbm1801 <cbm1801@naver.com>
2024-01-29 09:51:55 +0900

    resolve3 해답

--- Ch03_Control/src/ch03/continue11/Resolve3.java
+++ Ch03_Control/src/ch03/continue11/Resolve3.java
@@ -8,8 +8,9 @@ public class Resolve3 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
+		boolean isRun =true;
+		int money = 0; int m=0;
+		while(isRun) {
 			System.out.println("--------------------------");
 			System.out.println("1.예금 | 2.출금 3.잔고 | 4.종료");
 			System.out.println("--------------------------");
@@ -17,8 +18,28 @@ public class Resolve3 {
 			System.out.print("선택>");
 			
 			int a = sc.nextInt();
+			switch(a) {
+			case 1:
+				System.out.println("예금액>>");
+				m=sc.nextInt();
+				money += m;
+				break;
+			case 2:
+				System.out.println("출금액>>");
+				m=sc.nextInt();
+				money -= m;
+				break;
+			case 3:
+				System.out.println("잔고>>"+ money);
+				break;
+			case 4:
+				isRun=false;
+				break;
+			}
+		}
 			
-			if (a==1) {
+		System.out.println("프로그램 종료");	
+/*			if (a==1) {
 				System.out.println("예금액>"+10000);
 				System.out.println();
 			}	
@@ -35,10 +56,7 @@ public class Resolve3 {
 				break;
 			
 		}
-		System.out.println();
-		System.out.println("프로그램 종료");
-		
-		sc.close();
+*/
 	}
 
 }
