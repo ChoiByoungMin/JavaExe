e80e50e054476499e2f1f90a020c8a95bb99ef72
cbm1801 <cbm1801@naver.com>
2024-01-29 12:03:38 +0900

    주석 추가

--- Ch05_Array/src/ch05/array04/Resolve6.java
+++ Ch05_Array/src/ch05/array04/Resolve6.java
@@ -18,24 +18,66 @@ public class Resolve6 {
 			System.out.print("학생"+(i+1)+"의 점수를 입력하세요");
 			scores[i]=sc.nextInt();
 		
-				if(scores[i] >=90) 
-					System.out.println("A");
-				else if(scores[i]>=80)
-					System.out.println("B");
-				else if(scores[i]>=70)
-					System.out.println("C");
-				else if(scores[i]>=60)
-					System.out.println("D");
-				else 
-					System.out.println("F");
-										
+		}
+		for(int i=0; i<scores.length;i++) {
+		if(scores[i] >=90) 
+			System.out.println("A");
+		else if(scores[i]>=80)
+			System.out.println("B");
+		else if(scores[i]>=70)
+			System.out.println("C");
+		else if(scores[i]>=60)
+			System.out.println("D");
+		else 
+			System.out.println("F");
+								
 		System.out.println(names[i]+"학점"+ scores[i]);
 		System.out.println();
-		
 		}
-		
 		sc.close();
 				
 	}
 
 }
+
+//
+// 		String[] nameArr = new String[5];
+//		int[] scoreArr = new int[5];
+//		char[] chScoreArr = new char[5];
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		/* 5명의 학생 이름과 점수 입력*/
+//		for(int i=0;i<nameArr.length;i++) {
+//			System.out.print((i+1) + "번째 학생 이름 >> ");
+//			String name = sc.nextLine();
+//			System.out.print((i+1) + "번째 학생 점수 >> ");
+//			String strScore = sc.nextLine();
+//			int score = Integer.parseInt(strScore);
+//			
+//			nameArr[i] = name;
+//			scoreArr[i] = score;
+//		}
+//		
+//		/*점수를 학점으로 변환*/
+//		for(int i=0;i<scoreArr.length;i++) {
+//			char chScore;
+//			int score = scoreArr[i];
+//			if(score >= 90)
+//				chScore = 'A';
+//			else if(score >= 80)
+//				chScore = 'B';
+//			else if(score >= 70)
+//				chScore = 'C';
+//			else if(score >= 60)
+//				chScore = 'D';
+//			else
+//				chScore = 'F';
+//			
+//			chScoreArr[i] = chScore;
+//		}
+//		
+//		/*학생 이름과 학점 출력*/
+//		for(int i=0;i<nameArr.length;i++) {
+//			System.out.printf("%d %s:%c\n", 
+//							(i+1), nameArr[i], chScoreArr[i]);
