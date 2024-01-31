d7e380cf880a4a3c1bc1f7d05552bd97ec071591
cbm1801 <cbm1801@naver.com>
2024-01-31 16:12:57 +0900

    상속했을때 부모생성자 생성

--- Ch08_class/src/ch08/class09/Resolve2Main.java
+++ Ch08_class/src/ch08/class09/Resolve2Main.java
@@ -3,8 +3,7 @@ package ch08.class09;
 //클래스로 설계해서 프로그래밍 하세요
 public class Resolve2Main {
 	public static void main(String[] args) {
-		Resolve2Buy resolveBuy = new Resolve2Buy();
-		Resolve2Seller resolverSeller = new Resolve2Seller();
+		
 	}
 
 }
--- Ch08_class/src/ch08/class09/Resolve2Seller.java
+++ Ch08_class/src/ch08/class09/Resolve2Seller.java
@@ -2,9 +2,5 @@ package ch08.class09;
 //나는 마트에 가서 5000원을 주고 배 가격 2500를 샀다를
 //클래스로 설계해서 프로그래밍 하세요
 public class Resolve2Seller {
-	private int numPear;// 배 개수
-	private int sellerMoney;// 판매 금액
-	final int PEAR = 5000;// 배 고정 가격
 	
-
 }
