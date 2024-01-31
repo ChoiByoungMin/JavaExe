32b0db1e038724ab47cd2bfa6c471ab250f3297c
cbm1801 <cbm1801@naver.com>
2024-01-31 21:42:39 +0900

    문제 풀이

--- Ch08_class/src/ch08/class09/Resolve3.java
+++ Ch08_class/src/ch08/class09/Resolve3.java
@@ -16,5 +16,49 @@ package ch08.class09;
 // ABBA의 "Dancing Queen"노래를 Song객체로 생성하고
 // Show()로 출력하세요
 public class Resolve3 {
+	
+	private String title,artist,album,composer, year, track;
+	
+	public Resolve3 (String title,String artist, String album,
+			String composer,String year, String track) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		this.track = track;
+	}
+	// 노래의 정보를 저장하는 setSongInfo(...) : 관련된 매개변수를 인자로 넘겨줌
+	public void setSongInfo (String title,String artist, String album,
+			String composer,String year, String track) {
+		
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
+		this.track = track;
+		
+	}
+	
+	public void show() {
+		System.out.println("노래 정보를 열람합니다");
+		System.out.println("노래 : " + title);
+		System.out.println("가수 : " + artist);
+		System.out.println("앨범 제목 : " + album);
+		System.out.println("작곡가 : "+ composer);
+		System.out.println("노래가 발표된 연도 : "+year);
+		System.out.println("노래가 속한 앨범에서의 트랙 번호 : "+ track);
+		
+	}
+	public static void main(String[] args) {
+		
+		Resolve3 song = new Resolve3("Dancing Queen"
+				,"ABBA","arrival","벤뉘 안데르손, 비에른 울바에우스"
+				,"1976년 8월 15일 ","2번");
+		
+		song.show();
+		
+	}
 
 }
