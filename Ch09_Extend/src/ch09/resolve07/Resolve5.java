e345fdb18ebba7d0e6e7bc56c930a5f59e730770
cbm1801 <cbm1801@naver.com>
2024-02-01 15:53:02 +0900

    상속 -> 포함

--- Ch09_Extend/src/ch09/resolve07/Resolve5.java
+++ Ch09_Extend/src/ch09/resolve07/Resolve5.java
@@ -1,7 +1,13 @@
 package ch09.resolve07;
 
+/* 클래스 설계에서 접근권한 설정
+ * 1) 필드 -> private
+ *    메서드 -> public
+ * 2) 메서드 중에 내부에서만 사용하는 메서드 -> private  
+ * 
+ */
 public class Resolve5 {
-	int ballCnt;
+	private int ballCnt;
 	
 	public Resolve5(int ballCnt) {
 		this.ballCnt = ballCnt;
@@ -14,10 +20,10 @@ public class Resolve5 {
 		other.sub(cnt);
 		add(cnt);
 	}
-	public void add(int cnt) {
+	private void add(int cnt) {
 		this.ballCnt += cnt;
 	}
-	public void sub(int cnt) {
+	private void sub(int cnt) {
 		this.ballCnt -= cnt;
 	}
 	public int getBallCnt() {
