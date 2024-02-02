ebd37df739d60cbeaf9ccd5f97cc69a6baca7f9e
cbm1801 <cbm1801@naver.com>
2024-02-02 10:30:05 +0900

    정규사원 객체 생성

--- Ch09_Extend/src/ch09/abstract10/EmployeeManager.java
+++ Ch09_Extend/src/ch09/abstract10/EmployeeManager.java
@@ -10,10 +10,28 @@ public class EmployeeManager {
 	private Scanner sc = new Scanner(System.in);
 	
 	private int viewMenu() {
-		return 0;
+		System.out.println("[ 사원 선택 ]");
+		System.out.println("1. 정규직");
+		System.out.println("2. 임시직");
+		System.out.println("3. 일용직");
+		System.out.println("4. 전체정보보기");
+		System.out.println("5. 종료");
+		System.out.println("번호 입력 >>");
+		int sel = sc.nextInt();
+		return sel;
 	}
 	private RegularEmployee creatRegularEmployee() {
-		return null;
+		System.out.print("사번 >>");
+		String empno = sc.next();
+		System.out.print("이름 >>");
+		String name = sc.next();
+		System.out.print("연봉 >>");
+		int yearSalary = sc.nextInt();
+		System.out.print("보너스 >>");
+		int bonus = sc.nextInt();
+		RegularEmployee emp =
+				new RegularEmployee(empno, name, yearSalary,bonus);
+		return emp;
 	}
 	private TempEmployee creatTempEmployee() {
 		return null;
@@ -28,6 +46,41 @@ public class EmployeeManager {
 		
 	}
 	public void run() {
+		boolean isRun = true;
+		while(isRun) {
+			int selNum = viewMenu();
+			Employee emp = null;
+			switch(selNum) {
+			case EmpMenu.REG_EMP:
+				emp = creatRegularEmployee();
+				break;
+			case EmpMenu.TEMP_EMP:
+				emp = creatTempEmployee();
+				break;
+			case EmpMenu.PART_EMP:
+				emp = creatPartTimeEmployee();
+				break;
+			case EmpMenu.ALL_INFO:
+				emp = null;
+				viewAllEmployeeInfo();
+				break;
+			case EmpMenu.EXIT:
+				emp = null;
+				isRun = false;
+				break;
+			default:
+				emp = null;
+				System.out.println("해당 항목이 존재하지 않습니다.");
+				break;
+			}
+			//emp 객채가 존재한다면
+			if(emp != null) {
+				boolean isSave = saveEmployee(emp);
+				if(!isSave)
+					System.out.println("더 이상 저장 공간이 없습니다.");
+			}
+		}
+		System.out.println("Program Exit...");
 		
 	}
 
