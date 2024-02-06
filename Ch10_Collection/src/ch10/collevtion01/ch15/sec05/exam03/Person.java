b7ffa3c7d56a773a85f24c2d46d43940e90fe873
cbm1801 <cbm1801@naver.com>
2024-02-06 18:01:21 +0900

    Ch10.collevtion.jave 예제및 설명

--- Ch10_Collection/src/ch10/collevtion01/ch15/sec05/exam03/Person.java
+++ Ch10_Collection/src/ch10/collevtion01/ch15/sec05/exam03/Person.java
@@ -11,11 +11,16 @@ public class Person implements Comparable<Person>{
 
 	@Override
 	public int compareTo(Person o) {
-		if(age<o.age) return -1;
-		else if(age == o.age)return 0;
-		else return 1;
+		return name.compareTo(o.name)*-1; //역정렬
 	}
-	
+
+//	@Override	나이순서 정렬
+//	public int compareTo(Person o) {
+//		if(age<o.age) return -1;
+//		else if(age == o.age)return 0;
+//		else return 1;
+//	}
+
 	
 
 }
