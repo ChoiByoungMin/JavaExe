b8edd6d86a1b12863659f566526da990d9883931
cbm1801 <cbm1801@naver.com>
2024-02-07 18:19:58 +0900

    Generic.java

---
+++ Ch13_Generic/src/ch13/generic01/ch13/sec02/exam02/Home.java
@@ -0,0 +1,8 @@
+package ch13.generic01.ch13.sec02.exam02;
+
+public class Home {
+	public void turnOnLight() {
+		System.out.println("전등을 켭니다.");
+	}
+
+}
---
+++ Ch13_Generic/src/ch13/generic01/ch13/sec02/exam02/Rentable.java
@@ -0,0 +1,6 @@
+package ch13.generic01.ch13.sec02.exam02;
+
+public interface Rentable<P> {
+	P rent();
+
+}
