4651fd83bac7ebaafb480555defdeb38623cdbb3
cbm1801 <cbm1801@naver.com>
2024-02-13 12:35:33 +0900

    thread02.extendsThread 에문

--- Ch15_Thread/src/thread01/ch14/sec03/exam03/BeepPrintExample.java
+++ Ch15_Thread/src/thread01/ch14/sec03/exam03/BeepPrintExample.java
@@ -4,8 +4,12 @@ import java.awt.Toolkit;
 
 public class BeepPrintExample {
 	public static void main(String[] args) {
+		
+		// main 스레드(주인 아저씨)가 work스레드(알바생)을 고용한다.
 		Thread thread = new Thread() {
-
+			
+			
+			// 알바생이 해야할일 
 			@Override
 			public void run() {
 				Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -16,7 +20,7 @@ public class BeepPrintExample {
 			}
 			
 		};
-		
+		//worker스레드(알바생) 일 시작해
 		thread.start();
 		
 		for(int i =0; i<5;i++) {
