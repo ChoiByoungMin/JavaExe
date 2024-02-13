070d5ac7cadc336d2aae4a23fbd8d8e8406fc0fb
cbm1801 <cbm1801@naver.com>
2024-02-13 14:40:42 +0900

    thread. yield 예문

--- Ch15_Thread/src/thread01/ch14/sec05/exam02/SumThread.java
+++ Ch15_Thread/src/thread01/ch14/sec05/exam02/SumThread.java
@@ -13,6 +13,12 @@ public class SumThread extends Thread {
 	public void run() {
 		for(int i =0;i<=100;i++) {
 			sum+=i;
+//			try {
+//				sleep(10);
+//			} catch (InterruptedException e) {
+//				
+//				e.printStackTrace();
+//			}
 		}
 	}
 	
